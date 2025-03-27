36cd12efc19b3e26bc418bb6250260a2fb78c904
Ilyas <ilyas.elhallaoui@gmail.com>
2025-03-27 15:37:35 +0100

    again case statements

--- Bash-Kurs/case.sh
+++ Bash-Kurs/case.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 day=$(date +%a)
-case "$day" in
 
-Mon) echo "This is the backup for Monday" ;;
-Tue) echo "This is the backup for Tuesday" ;;
-Wed) echo "This is the backup for Wednesday" ;;
-Thu) echo "This is the backup for Thursday" ;;
-Fri) echo "This is the backup for Friday" ;;
+case "$day" in
+Mon|Mo|Fri|Fre|Wed|Mit)		echo "This is the backup for Server1" ;;
+Tue|Die|Thu|Die)		echo "This is the backup for Server2" ;;
+*)				echo "This is the backup for the whole week" ;;
 esac
 
+case $1 in
+Yes|yes|YES|yES|yeS|YEs)	echo "The order is executed";;
+No|NO|no|nO)			echo "The order is not executed";;
+*)				echo "There are no orders given, choose Yes or No";;
+esac
