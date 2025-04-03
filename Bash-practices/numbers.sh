1349568cf8f1529c5a60e69e65cba8ab3a3d3286
Ilyas <ilyas.elhallaoui@gmail.com>
2025-04-03 14:04:42 +0200

    Übungsaufgaben

---
+++ Bash-practices/numbers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ 10 -ge 10 ]; then
+	echo "Die Zahl ist größer oder gleich 10"
+else
+	echo "Die Zahl ist kleiner als 10"
+fi
+
---
+++ Bash-practices/strings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+string1="Hallo"
+string2="Welt"
+
+if [ "$string" = "$string2" ]; then
+	echo "Die Strings sind gleich"
+else
+	echo "Die Strings sind ungleich"
+fi
