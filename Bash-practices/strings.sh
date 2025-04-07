fa76d3f9f50066fa8dc04e345108aaaa2ac1700a
Ilyas <ilyas.elhallaoui@gmail.com>
2025-04-07 10:16:59 +0200

    improved mistake in string.sh

--- Bash-practices/strings.sh
+++ Bash-practices/strings.sh
@@ -3,7 +3,7 @@
 string1="Hallo"
 string2="Welt"
 
-if [ "$string" = "$string2" ]; then
+if [ "$string1" = "$string2" ]; then
 	echo "Die Strings sind gleich"
 else
 	echo "Die Strings sind ungleich"
